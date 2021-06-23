fad4524b7cb5dbbc26ab8972252b5015b3d2fead
Lashaun <clarklas831@gmail.com>
2021-06-23 17:57:33 -0500

    task audit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
+
   // append to ul list on the page
   $('#list-' + taskList).append(taskLi);
 };
@@ -132,11 +134,18 @@ $(`.list-group`).on(`click`, `span`, function () {
 
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      $(this).trigger(`change`);
+    },
+  });
+
   // focus on new element
   dateInput.trigger(`focus`);
 });
 
-$(`.list-group`).on(`blur`, `input[type="text"]`, function () {
+$(`.list-group`).on(`change`, `input[type="text"]`, function () {
   //prettier-ignore
   let date = $(this)
     .val()
@@ -162,6 +171,9 @@ $(`.list-group`).on(`blur`, `input[type="text"]`, function () {
 
   // replace the input with the new span
   $(this).replaceWith(taskSpan);
+
+  // pass the li element into auditTask() to check new date
+  auditTask($(taskSpan).closest(`.list-group-item`));
 });
 
 $(`.card .list-group`).sortable({
@@ -229,5 +241,30 @@ $(`#trash`).droppable({
     console.log(`out`);
   },
 });
+
+$(`#modalDueDate`).datepicker({
+  minDate: 1,
+});
+
+let auditTask = function (taskEl) {
+  //prettier-ignore
+  let date = $(taskEl)
+  .find(`span`)
+  .text()
+  .trim();
+  console.log(date);
+
+  let time = moment(date, `L`).set(`hour`, 17);
+  console.log(time);
+
+  $(taskEl).removeClass(`list-group-item-warning list-group-item-danger`);
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass(`list-group-item-danger`);
+  } else if (Math.abs(moment().diff(time, `days`)) <= 2) {
+    $(taskEl).addClass(`list-group-item-warning`);
+  }
+};
+
 // load tasks for the first time
 loadTasks();
